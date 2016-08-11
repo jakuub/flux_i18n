7f094e9b8622b8eb14310c66c52e62fca8bf7a7d
Jakuub Uhrik <jakuub@mail.com>
2016-08-11 12:25:05 +0200

    Added placeholders

--- lib/localized_component.dart
+++ lib/localized_component.dart
@@ -2,36 +2,51 @@ library localized_component;
 
 import 'package:flux/component.dart';
 import 'package:vacuum_persistent/persistent.dart';
+
 import 'common.dart';
 
 class LocalizedComponent extends Component<PersistentIndexedCollection> {
+
+  final String placeholderStart = "{{";
+  final String placeholderEnd = "}}";
 
   @override
   PersistentIndexedCollection get data => lookupIn(super.data, [DATA]);
 
   PersistentIndexedCollection get locale => lookupIn(super.data, [LOCALE]);
 
-  LocalizedComponent(Props<PersistentIndexedCollection> props, [children]) : super(props, children);
+  LocalizedComponent(Props<PersistentIndexedCollection> props, [children])
+      : super(props, children);
 
-  String locate(String key, {bool global: false}) {
-
+  String locate(String key, {bool global: false, Map placeholders}) {
     String result = lookupIn(locale, [key]);
 
     if (result != null && !global) {
-      return result;
+      return addPlaceholders(result, placeholders);
     }
 
     if (lookupIn(locale, [GLOBAL]) != null) {
-      return lookupIn(locale, [GLOBAL, key]);
+      return addPlaceholders(lookupIn(locale, [GLOBAL, key]), placeholders);
     }
 
     return null;
   }
 
+  addPlaceholders(String locale, Map placeholders) {
+    if (placeholders == null || locale == null) {
+      return locale;
+    }
+
+    return placeholders.keys.fold(locale, (previous, key) =>
+        locale.replaceAll(
+            placeholderStart + key + placeholderEnd, placeholders[key]));
+  }
+
   /**
    * Helper for easier usage
    */
-  String l(String key, {bool global: false}) => locate(key, global: global);
+  String l(String key, {bool global: false, Map placeholders}) =>
+      locate(key, global: global, placeholders: placeholders);
 
   @override
   Props createProps(PersistentIndexedCollection data, {String name}) {
@@ -42,11 +57,13 @@ class LocalizedComponent extends Component<PersistentIndexedCollection> {
   }
 
   @override
-  Props cp(PersistentIndexedCollection data, {String name}) => createProps(data, name: name);
+  Props cp(PersistentIndexedCollection data, {String name}) =>
+      createProps(data, name: name);
 
   PersistentIndexedCollection _chooseLocale(String name) {
     if (name != null) {
-      return insertIn(lookupIn(locale, [name], notFound: persist({})), [GLOBAL], lookupIn(locale, [GLOBAL], notFound: persist({})));
+      return insertIn(lookupIn(locale, [name], notFound: persist({})), [GLOBAL],
+          lookupIn(locale, [GLOBAL], notFound: persist({})));
     }
 
     return locale;
